eaee8d0d763a529dcdfa124406516826dd756b59
zap8600 <95487746+zap8600@users.noreply.github.com>
2025-05-21 13:15:36 -0500

    wip

--- efiboot/boot.s
+++ efiboot/boot.s
@@ -4,6 +4,10 @@
 _start:
     // Load the ELF data into arguments
     // rip is used for position-independent code
+    lea ImageBase(%rip), %rdi
+    lea _DYNAMIC(%rip), %rsi
+    call initefi
+    ret
 
 // Fake relocation info so EFI won't complain
     .data
@@ -15,4 +19,17 @@ label1:
     .word 0
 
     .text
+    .globl initefi
+initefi:
+    pushq %rbp
+    movq %rsp, %rbp
+    subq $32, %rsp
+    movq $0, %rax // Using this to store loop counter
+_loop:
+    movq (%rsi, %r8, 16), %r8
+    movq $0, %r9
+    cmp, %r8, %r9
+    // Jump out of the loop if NULL
+    // jz something, something
+    
 
