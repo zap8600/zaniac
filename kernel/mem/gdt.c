3e259128ecd1cc78ff9ad714d8d57df02ba31719
zap8600 <95487746+zap8600@users.noreply.github.com>
2025-10-09 17:00:05 +0000

    wip

--- kernel/mem/gdt.c
+++ kernel/mem/gdt.c
@@ -1,1 +1,10 @@
 #include "gdt.h"
+
+typedef struct gdtentry_t {
+    unsigned short int limitlow;
+    unsigned short int baselow;
+    unsigned char basemiddle;
+    unsigned char access;
+    unsigned char granularity;
+    unsigned char basehigh;
+} __attribute__((packed)) gdtentry_t;
