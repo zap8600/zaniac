0cfe1d084d8ac6950c0a00ea903819c4349f4481
zap8600 <zapost86@gmail.com>
2025-10-28 14:48:20 -0500

    works!

--- boot/riscv64/efi/main.c
+++ boot/riscv64/efi/main.c
@@ -3,21 +3,8 @@
 #include "VGA8.h"
 #include "efi.h"
 
-efigop_t* gop = (void*)0;
-
-void clearscr() {
-    for(unsigned int y = 0; y < gop->mode->info->vres; y++) {
-        for(unsigned int x = 0; x < gop->mode->info->hres; x++) {
-            gop->mode->fbbase[(y * gop->mode->info->pixperscanline) + x] = 0x00000000;
-        }
-    }
-}
-
-unsigned int cx = 0;
-unsigned int cy = 0;
-char* uart = (void*)0x10000000;
 void wchscr(const char c) {
-    *uart = c;
+    *((unsigned char*)0x10000000) = (unsigned char)c;
 }
 
 void wstr(const char* s) {
@@ -49,7 +36,7 @@ void bootstrap(void) {
         "    .2byte 0x206\n"
         "optional_header:\n"
         "    .2byte 0x20b\n"
-        "    .byte 0x2\n"
+        "    .byte 0x02\n"
         "    .byte 0x14\n"
         "    .4byte _text_size - image_base\n"
         "    .4byte _alldata_size - image_base\n"
@@ -66,7 +53,7 @@ void bootstrap(void) {
         "    .2byte 0\n"
         "    .2byte 0\n"
         "    .2byte 0\n"
-        "    .2byte 0\n"
+        "    .4byte 0\n"
         "    .4byte _image_end - image_base\n"
         "    .4byte _text - image_base\n"
         "    .4byte 0\n"
@@ -107,17 +94,6 @@ void bootstrap(void) {
         "    .2byte 0\n"
         "    .4byte 0x60000020\n"
 
-        "    .ascii \".reloc\\0\\0\"\n"
-        "    .4byte _reloc_vsize - image_base\n"
-        "    .4byte _reloc - image_base\n"
-        "    .4byte _reloc_size - image_base\n"
-        "    .4byte _reloc - image_base\n"
-        "    .4byte 0\n"
-        "    .4byte 0\n"
-        "    .2byte 0\n"
-        "    .2byte 0\n"
-        "    .4byte 0x42000040\n"
-
         "    .ascii \".data\\0\\0\\0\"\n"
         "    .4byte _data_vsize - image_base\n"
         "    .4byte _data - image_base\n"
@@ -129,6 +105,17 @@ void bootstrap(void) {
         "    .2byte 0\n"
         "    .4byte 0xC0000040\n"
 
+        "    .ascii \".reloc\\0\\0\"\n"
+        "    .4byte _reloc_vsize - image_base\n"
+        "    .4byte _reloc - image_base\n"
+        "    .4byte _reloc_size - image_base\n"
+        "    .4byte _reloc - image_base\n"
+        "    .4byte 0\n"
+        "    .4byte 0\n"
+        "    .2byte 0\n"
+        "    .2byte 0\n"
+        "    .4byte 0x42000040\n"
+
         "    .ascii \".rodata\\0\"\n"
         "    .4byte _rodata_vsize - image_base\n"
         "    .4byte _rodata - image_base\n"
@@ -139,10 +126,14 @@ void bootstrap(void) {
         "    .2byte 0\n"
         "    .2byte 0\n"
         "    .4byte 0x40000040\n"
-
         "    .text\n"
         "    .globl _start\n"
         "_start:\n"
+        // "    lui a0, 0x10000\n" // setup uart addr
+        // "    addi a1, zero, 72\n" // set a1 to 'H'
+        // "_loop_print:\n"
+        // "    sb a1, 0(a0)\n" // print character
+        // "    bne a1, a0, _loop_print\n" 
         "    lla a2, image_base\n"
         "    lla a3, _DYNAMIC\n"
         "    call inituefi\n"
@@ -158,6 +149,15 @@ void bootstrap(void) {
     );
 }
 
+void* memcpy(void* d, void* s, unsigned long long n) {
+    unsigned char* dst = (unsigned char*)d;
+    const unsigned char* src = (const unsigned char*)s;
+    for(unsigned long long i = 0; i < n; i++) {
+        dst[i] = src[i];
+    }
+    return d;
+}
+
 typedef struct elf64dyn_t {
     signed long long dtag;
     union {
@@ -169,46 +169,54 @@ typedef struct elf64dyn_t {
 typedef struct elf64rel_t {
     unsigned long long roffset;
     unsigned long long rinfo;
+    signed long long raddend;
 } elf64rel_t;
 
 unsigned long long inituefi(void* image, efisystemtable_t* systab, unsigned long long ldbase, elf64dyn_t* dyn) {
-    // long long relsz = 0;
-    // long long relent = 0;
-    // elf64rel_t* rel = (void*)0;
-    // unsigned long long* addr;
-    // for(int i = 0; dyn[i].dtag != 0; i++) {
-    //     switch(dyn[i].dtag) {
-    //         case 7: {
-    //             rel = (elf64rel_t*)(dyn[i].dun.dptr + ldbase);
-    //             break;
-    //         }
-    //         case 8: {
-    //             relsz = dyn[i].dun.dval;
-    //             break;
-    //         }
-    //         case 9: {
-    //             relent = dyn[i].dun.dval;
-    //             break;
-    //         }
-    //         default: break;
-    //     }
-    // }
-    // if(rel && relent) {
-    //     while(relsz > 0) {
-    //         if(((rel->rinfo) & 0xffffffff) == 3) {
-    //             addr = (unsigned long long*)(ldbase + rel->roffset);
-    //             *addr += ldbase;
-    //         }
-    //         rel = (elf64rel_t*)(((char*)rel) + relent);
-    //         relsz -= relent;
-    //     }
-    // }
+    long long relsz = 0;
+    long long relent = 0;
+    elf64rel_t* rel = (void*)0;
+    unsigned long long* addr;
+    for(int i = 0; dyn[i].dtag != 0; i++) {
+        switch(dyn[i].dtag) {
+            case 7: {
+                rel = (elf64rel_t*)(dyn[i].dun.dptr + ldbase);
+                break;
+            }
+            case 8: {
+                relsz = dyn[i].dun.dval;
+                break;
+            }
+            case 9: {
+                relent = dyn[i].dun.dval;
+                break;
+            }
+            default: break;
+        }
+    }
+    if(rel && relent) {
+        while(relsz > 0) {
+            if(((rel->rinfo) & 0xffffffff) == 3) {
+                addr = (unsigned long long*)(ldbase + rel->roffset);
+                *addr += ldbase + rel->raddend;
+            }
+            rel = (elf64rel_t*)(((char*)rel) + relent);
+            relsz -= relent;
+        }
+    }
+    // asm volatile(
+    //     "_loop_print:\n"
+    //     "    addi a0, zero, 72\n" // set a0 to 'H'
+    //     "    addi a1, zero, 1\n" // set a1 to 1
+    //     "    call wchscr\n" // Print character
+    //     "    bne a1, a0, _loop_print\n" 
+    // );
 
     // unsigned long long status = 0;
 
-    //wstr("Hello, RISC-V world!\n");
+    // wstr("Hello, RISC-V world!\n");
     
-    // while(1) {}
+    while(1) { wchscr(72); }
 
     return 0;
 }
